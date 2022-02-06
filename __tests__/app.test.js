ee02d7cb2a86060df9543d41b58a164c7d53256b
maddenlockin <maddenlockin@gmail.com>
2022-02-06 11:15:33 -0500

    post word to db- passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,28 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const newWord = {
+  word: 'word',
+  aka: 'wo',
+  definition: 'a word',
+  reason: 'its a word',
+  origin: 'transphobia',
+  alternatives: 'sentence'
+};
+
 describe('vocab-api routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('posts word to db', async () => {
+    const res = await request(app)
+      .post('/api/v1/words/create')
+      .send(newWord);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...newWord,
+    });
   });
 
   afterAll(() => {

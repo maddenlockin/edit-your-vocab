aecb019b5b988387d74914116e64e76702dc5292
maddenlockin <maddenlockin@gmail.com>
2022-02-06 10:59:54 -0500

    sql table initialzed with seed data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,24 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS words;
+
+CREATE TABLE words (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  word TEXT NOT NULL,
+  aka TEXT,
+  definition TEXT,
+  reason TEXT,
+  origin TEXT,
+  alternatives TEXT
+);
+INSERT INTO words (word, aka, definition, reason, origin, alternatives) VALUES 
+('dumb', null, 'unable to speak', 'deaf and dumb', '', ''),
+('crazy', null, 'mentally unstable or unpredictable', '', '', 'ridiculous, outrageous, confusing'),
+('psychotic', 'psycho', '', '', '', 'ridiculous, outrageous, confusing'),
+('insane', null, '', '', '', 'ridiculous, outrageous, confusing'),
+('idiot', null, '', '', '', 'not smart'),
+('imbecile', null, '', '', '', 'not smart'),
+('cripple', 'crip', 'a physically disabled person', 'slur against physically disabled people', '', ''),
+('crack-head', 'meth-head', 'a drug user', 'minimizes the disease of addiction and consolidates a human being as one piece of their life', '', 'person, neighbor'),
+('clean (in relation to STI status)', null, 'a negative result for an STI screening', 'Conotates that a positive result means someone is dirty, stigmatizes STI positive people including people living with HIV.', '', 'negative'),
+('lame', null, '', '', '', ''),
+('maniac', null, '', '', '', '')
+;
